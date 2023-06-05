b4c9811d6271ca3159dc1e977b87dd95f7452737
Khaetbek <hayotbek-web@mail.ru>
2023-06-05 07:17:33 +0500

    #fix #token

--- js/insta.js
+++ js/insta.js
@@ -1,6 +1,6 @@
 const InitInstaFunc = () => {
     const feed = new Instafeed({
-        accessToken: 'IGQVJXWk5sd2hETTBLeWYxVnpwSmE0ZA1dVT3hmNmxtNFZAqZAU1PUnZAmM29IWHM5bXlPSTdjZAFZAoU1Bnd2R0Qlo4ZAHNZAa0FXY1ZAfb1p4VGkya3p2VlAyMDJ6WWcwSXdkXzdfVC1aOHhLVVlEc3gwakx4WgZDZD'
+        accessToken: 'IGQVJXalE1MHBXV2dzclJHWUNRTDVoLThILXUxNDB0MmxMX0R5b1c3Tzg5U1dVdUxpb2U0c3RRS3AzMnlqTFd1X19jUGhHV0NWZAG05bXVkQnVZAX0xPeno4YkRyTmgyMldlLU9PbG9TLWFWcTlySVZA2agZDZD'
     });
     feed.run();
 
